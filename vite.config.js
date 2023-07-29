7c65e86a8228846f87dbb82c8dc34eb0cf0fbe0b
JeffrinR <jeffrinromerom@gmail.com>
2023-07-29 13:54:00 -0600

    Convert to pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import workbox from 'workbox-webpack-plugin';
+
 export default defineConfig({
+  // ...otras configuraciones...
   plugins: [vue()],
-})
+
+  build: {
+    rollupOptions: {
+      // ...otras opciones de rollup...
+      output: {
+        // ...otras opciones de salida...
+        manualChunks: undefined,
+      },
+    },
+  },
+
+  plugins: [
+    // ...otros complementos...
+    workbox({
+      swDest: './dist/sw.js',
+      globDirectory: './dist',
+      globPatterns: ['**/*.{html,js,css}'],
+    }),
+  ],
+});
